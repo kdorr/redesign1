164d13cc2d477e907bda5349a50999c7b17506f1
kdorr <kdorr@users.noreply.github.com>
2018-01-27 10:46:36 -0600

    Clean up pressing visual issues

--- redesign.py
+++ redesign.py
@@ -1,6 +1,7 @@
 import pandas
+from math import pi
 from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Range1d
 
 #Read data
 df = pandas.read_csv("data.csv")
@@ -16,9 +17,10 @@ perc = perc.sort_values('dollars', ascending=False)
 src = ColumnDataSource(perc)
 #Setup plot
 plot = figure(
-    x_axis_label="Organization", y_axis_label="dollars", x_range=[row[0] for row in perc.values]
+    plot_width=600, plot_height=600, x_axis_label="Organization", y_axis_label="percentage of funding", x_range=[row[0] for row in perc.values], y_range=Range1d(0,1)
 )
 
 plot.vbar(x='Organization', top='dollars', width=.5, source=src)
-plot.y_range.start=0
+plot.yaxis.bounds=(0,1)
+plot.xaxis.major_label_orientation = pi/2
 show(plot)
