7b2ef72cea0f430d7acd52974215d4aa048736b6
kdorr <kdorr@users.noreply.github.com>
2018-02-10 15:57:43 -0600

    Add raw data plot

--- redesign.py
+++ redesign.py
@@ -2,28 +2,45 @@ import pandas
 from math import pi
 from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource, Range1d
+from bokeh.layouts import column, row, gridplot
 
 #Read data
 df = pandas.read_csv("data.csv")
+
+#Raw data
+raw = df
+raw = raw.sort_values('dollars', ascending=False)
+raw_src = ColumnDataSource(raw)
+
+raw_plot = figure(
+    plot_width=600, plot_height=600,
+    x_axis_label="Funding (in dollars)", y_axis_label="Organization",
+    y_range=[row[0] for row in raw.values]
+)
+raw_plot.hbar(y='Organization', right='dollars',
+              height=.5, color="#718dbf",
+              source=raw_src)
 
 #Percentages
 sum = df['dollars'].sum()
 perc = df
 perc['dollars'] = df['dollars']/sum
 perc['remainder'] = 1 - df['dollars']
-
-#Format data
-#src = ColumnDataSource(df)
 perc = perc.sort_values('dollars', ascending=False)
 src = ColumnDataSource(perc)
 
 #Setup plot
-plot = figure(
+perc_plot = figure(
     plot_width=600, plot_height=600,
-    x_axis_label="Percentage of Funding", y_axis_label="Organization",
+    x_axis_label="Percentage of Funding", #y_axis_label="Organization",
     x_range=Range1d(0,1), y_range=[row[0] for row in perc.values]
 )
 
-plot.hbar_stack(['dollars', 'remainder'], y='Organization', height=.5, color=["#718dbf","#c9d9d3"], source=src)
+perc_plot.hbar_stack(['dollars', 'remainder'], y='Organization',
+                     height=.5, color=["#718dbf","#c9d9d3"],
+                     source=src)
 
-show(plot)
+#show(row(raw_plot, perc_plot))
+grid = gridplot([[raw_plot, perc_plot]])
+show(grid)
+#show(raw_plot)
