cd47d7b6c74a99eff134f2635453e697dd05eb61
kdorr <kdorr@users.noreply.github.com>
2018-02-11 10:31:15 -0600

    fix x-axes

--- redesign.py
+++ redesign.py
@@ -1,7 +1,7 @@
 import pandas
 from math import pi
 from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource, Range1d
+from bokeh.models import ColumnDataSource, Range1d, NumeralTickFormatter
 from bokeh.layouts import gridplot
 
 #Read data
@@ -16,11 +16,12 @@ raw_plot = figure(
     plot_width=600, plot_height=600,
     title="Tier One Organization Budget Breakdown (raw numbers)",
     x_axis_label="Funding (in dollars)", y_axis_label="Organization",
-    y_range=[row[0] for row in raw.values]
+    x_range=Range1d(0, 170000), y_range=[row[0] for row in raw.values]
 )
 raw_plot.hbar(y='Organization', left=0, right='dollars',
               height=.5, color="#718dbf",
               source=raw_src)
+raw_plot.xaxis[0].formatter = NumeralTickFormatter(format="$0,000")
 
 #Percentages
 sum = df['dollars'].sum()
@@ -34,14 +35,14 @@ src = ColumnDataSource(perc)
 perc_plot = figure(
     plot_width=600, plot_height=600,
     title="Percentage of Tier One Budget by Organization",
-    x_axis_label="Percentage of Funding", y_axis_label="Organization",
+    x_axis_label="Percentage of Whole Tier One Budget", y_axis_label="Organization",
     x_range=Range1d(0,1), y_range=[row[0] for row in perc.values]
 )
 
 perc_plot.hbar_stack(['dollars', 'remainder'], y='Organization',
                      height=.5, color=["#718dbf","#c9d9d3"],
                      source=src)
-
+perc_plot.xaxis[0].formatter = NumeralTickFormatter(format="0%")
 
 grid = gridplot([[raw_plot], [perc_plot]])
 show(grid)
